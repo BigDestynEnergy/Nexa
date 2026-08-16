8eec742bf4f966a164673c4e2c280f78026327aa
Destyn <destynbanda@gmail.com>
2026-08-16 20:23:26 +0200

    Add AppLayout component with Topbar and Outlet

--- src/Routes/Layout.jsx
+++ src/Routes/Layout.jsx
@@ -1,1 +1,12 @@
+import Topbar from "../Components/Topbar";
+import {Outlet} from "react-router-dom";
 
+export default function AppLayout(){
+  
+  return(
+    <div className="app-layout">
+    <Topbar/>
+      <Outlet/>
+    </div>
+  )
+}
